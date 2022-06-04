fd854e80b8b04c7f9c10894a3c803067e275be39
Harshal Lele <harshallele97@gmail.com>
2022-06-04 22:30:02 +0530

    place autocomplete query

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
-import logo from './logo.svg';
+import { Autocomplete } from '@mui/material';
+import { TextField } from '@mui/material';
+import { useEffect, useState } from 'react';
+import debounce from "lodash"
+
 import './App.css';
 
 function App() {
+  const [options,setOptions] = useState([]);
+  const [inputVal, setInputVal] = useState("");
+
+  const getAutocompleteSuggestions = (input) => {
+    if(typeof window?.google !== 'object') 
+      return;
+    
+    const service = new window.google.maps.places.AutocompleteService();
+    service.getPlacePredictions({ input }, (pred, status) => {
+  
+      if(typeof window?.google !== 'object') 
+        return;
+  
+      if (status != window.google.maps.places.PlacesServiceStatus.OK || !pred || !pred.length) {
+        setOptions([]);
+        console.log("Autocomplete query status:", status);
+      }
+      setOptions(pred);
+
+    });
+  }
+
+  useEffect(() => {
+    if(typeof inputVal !== "string" || inputVal?.trim() === ""){
+      setOptions([]);
+      return;
+    }
+    getAutocompleteSuggestions(inputVal);
+  }, [inputVal]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Enter location
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Autocomplete
+          filterOptions={(a) => a}
+          sx={{width: 600}}
+          options={options.map(o => o.description)}
+          renderInput={(params) => (
+            <TextField {...params} label="Add a location" fullWidth />
+          )}
+          onInputChange={(e) => {
+            setInputVal(e.target.value);
+          }}
+        />
       </header>
     </div>
   );
