f82c22eaf72409856cdb32acc5902c6b49c75eb3
Harshal Lele <harshallele97@gmail.com>
2022-06-04 22:30:45 +0530

    geocode selected option

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './App.css';
 function App() {
   const [options,setOptions] = useState([]);
   const [inputVal, setInputVal] = useState("");
+  const [placeDetail, setPlaceDetail] = useState({});
 
   const getAutocompleteSuggestions = (input) => {
     if(typeof window?.google !== 'object') 
@@ -28,6 +29,23 @@ function App() {
     });
   }
 
+  const getSelectedPlaceInfo = (place) => {
+    if(typeof place !== "object" || typeof window?.google !== "object")
+      return;
+
+    console.log(place);
+    const geocoder = new window.google.maps.Geocoder();
+
+    geocoder
+      .geocode({placeId: place.place_id})
+      .then((results, status) => {
+        console.log(results,status);
+        if(results.results){
+          setPlaceDetail(results.results[0]);
+        }
+      });
+  };
+
   useEffect(() => {
     if(typeof inputVal !== "string" || inputVal?.trim() === ""){
       setOptions([]);
@@ -45,14 +63,40 @@ function App() {
         <Autocomplete
           filterOptions={(a) => a}
           sx={{width: 600}}
-          options={options.map(o => o.description)}
+          options={options.map(a => a.description)}
           renderInput={(params) => (
             <TextField {...params} label="Add a location" fullWidth />
           )}
           onInputChange={(e) => {
             setInputVal(e.target.value);
           }}
+          onChange={(e, value, details) => {
+            
+            let selOption = options?.filter(a => a.description === value)?.[0] || {};
+            getSelectedPlaceInfo(selOption);
+          }}
         />
+        <div style={{ textAlign: 'left' }}>
+          <div style={{marginTop: '50px'}}>
+            Full address
+          </div>
+          <div style={{color: 'grey'}}>
+            {placeDetail?.formatted_address}
+          </div>
+          <div>
+            Place type
+          </div>
+          <div style={{color: 'grey'}}>
+            {placeDetail?.types?.join(",")}
+          </div>
+          <div>
+            Coordinates
+          </div>
+          <div style={{color: 'grey'}}>
+            {placeDetail?.geometry.location.lat()},{placeDetail?.geometry.location.lng()}
+          </div>
+        </div>
+
       </header>
     </div>
   );
